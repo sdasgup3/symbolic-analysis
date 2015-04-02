8acf2cc57d8de3340e5c003e4035aa26abef180f
Sandeep Dasgupta <sdasgup3@illinois.edu>
2015-04-02 05:12:08 -0500

    simplified version of 14401

---
+++ Testcases/CustomTests/zesti_14401_simple.c
@@ -0,0 +1,32 @@
+#include <string.h>
+#include"klee.h"
+
+
+#include <getopt.h>
+
+struct S {
+  int x, y; 
+};
+
+struct S data[] =
+  {
+    { 1,2 },
+    { 3,4 },
+  };
+
+int main(int argc, char** argv) {
+
+  int i, x, S;
+  x= 0 ;
+  struct S* z;
+
+  klee_make_symbolic(&x, sizeof(x), "X");
+  klee_assume(x >= 0 && x <= 1 );
+
+  for(i = 0 ; i < 1 ; i ++) {
+    z   = &data[x++];
+    S = z->x ; 
+  }
+
+  return 0;
+}
