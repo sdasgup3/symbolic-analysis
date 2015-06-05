4568c98052ac88c11518dda6995f3451fdd98c8d
Sandeep Dasgupta <sdasgup3@illinois.edu>
2015-06-04 20:27:56 -0500

    script chages for the new

--- Scripts/build.pl
+++ Scripts/build.pl
@@ -11,10 +11,12 @@ my $llvmpalib       = "/home/sdasgup3/SymbolicAnalysis/zesti/Release+Asserts/lib
 ##############################################
 
 my $test = "";
+my $testll = "";
 my $maxt = "";
 my $result = "";
 my $reuse = "";
 my $genexec = "";
+my $dsa = "";
 my $run = "";
 my $watch = "";
 my $modifiedll = "";
@@ -22,35 +24,41 @@ my $withoutcheker = "";
 my @progargs = "";
 my $zest = "1";
 my $offset = "3";
-my $klee_dir = "";
+my $klee_src = "";
+my $klee_obj = "";
 my $llvm_dir = "";
 
 GetOptions ("wc"        => \$withoutcheker, 
             "test=s"    => \$test, 
+            "testll=s"    => \$testll, 
             "offset=s"  => \$offset, 
             "zest"      => \$zest, 
             "result"    => \$result, 
             "reuse"     => \$reuse, 
             "genexec"   => \$genexec, 
+            "dsa"       => \$dsa, 
             "run"       => \$run, 
             "watch"     => \$watch, 
             "args=s"    => \@progargs, 
             "maxt=s"    => \$maxt, 
-            "klee_dir=s"    => \$klee_dir, 
+            "klee_src=s"    => \$klee_src, 
+            "klee_obj=s"    => \$klee_obj, 
             "llvm_dir=s"    => \$llvm_dir, 
             "mdll"      => \$modifiedll) 
  or die("Error in command line arguments\n");
 
 
-if("" eq $test) {
-  print "Specify -test [-zest offset=N] [-wc -mdll] -klee_dir -llvm_dir \n";
-  print "Usage: ~/SymbolicAnalysis/Scripts/build.pl -wc -zest -offset 3 -klee_dir ~/SymbolicAnalysis/zesti/ -llvm_dir ~/llvm/llvm-3.4.2/llvm-build/ -test ?\n";
+if("" eq $test && "" eq $testll) {
+  print "Specify -test [-zest offset=N] [-wc -mdll] -klee_src -klee_obj -llvm_dir \n";
+  print "Usage: ~/SymbolicAnalysis/Scripts/build.pl -wc -zest -offset 3 -klee_src ~/SymbolicAnalysis/zesti/ -klee_obj ~/SymbolicAnalysis/zesti-obj/ -llvm_dir ~/llvm/llvm-3.4.2/llvm-build/ -test ?\n";
   exit(1);
 }
 
-if("" eq $klee_dir) {
-  $klee_dir = "~/SymbolicAnalysis/zesti/";
-  print "Using klee_dir as $klee_dir\n";
+if("" eq $klee_src || "" eq $klee_obj) {
+  $klee_src = "~/SymbolicAnalysis/zesti/";
+  $klee_obj = "~/SymbolicAnalysis/zesti-obj/";
+  print "Using klee_src as $klee_src\n";
+  print "Using klee_obj as $klee_obj\n";
 }
 if("" eq $llvm_dir) {
   $llvm_dir = "~/llvm/llvm-3.4.2/llvm-build/";
@@ -68,8 +76,8 @@ my $clang       = "$llvm_bin/clang";
 my $llvmdis     = "$llvm_bin/llvm-dis";
 my $llvmas      = "$llvm_bin/llvm-as";
 my $llvmld      = "$llvm_bin/llvm-ld";
-my $palib       = "$klee_dir/lib/";
-my $painclude   = "$klee_dir/include/";
+my $palib       = "$klee_src/lib/";
+my $painclude   = "$klee_src/include/";
 
 if(!(-e "$llvmld")) { # We llvm-ld not there, so search for llvm-link
   $llvmld = "$llvm_bin/llvm-link";
@@ -80,8 +88,8 @@ if(!(-e "$llvmld")) { # We llvm-ld not there, so search for llvm-link
 ###  Klee/Zesti Args
 my $runkleetest = "$SCRIPTDIR/runseq";
 my $watchV      = "$SCRIPTDIR/watchV";
-my $kleeexec    = "$klee_dir/Release+Asserts/bin/klee";
-my $kleeincl    = "$klee_dir/include/klee";
+my $kleeexec    = "$klee_obj/Release+Asserts/bin/klee";
+my $kleeincl    = "$klee_src/include/klee";
 my $maxtime     = "";
 my $kleeargs    = "";
 
@@ -107,43 +115,29 @@ if($zest eq "") {
       . " --randomize-fork --search=random-path --search=nurs:covnew" 
       . " --use-batching-search --batch-instructions=10000"; 
 } else {
-#$kleeargs = " --zest  -aachecks  --disable-opt --debug-print-aachecks -debug-print-instructions   --zest-depth-offset=$offset       --use-symbex=2 --symbex-for=10 --search=zest --zest-search-heuristic=br --zest-discard-far-states=false"; 
-  $kleeargs = " --zest            --disable-opt                        -debug-print-instructions   --zest-depth-offset=$offset       --use-symbex=2 --symbex-for=10 --search=zest --zest-search-heuristic=br --zest-discard-far-states=false"; 
+  $kleeargs = " --zest  -aachecks  --disable-opt --debug-print-aachecks -debug-print-instructions   --zest-depth-offset=$offset       --use-symbex=2 --symbex-for=10 --search=zest --zest-search-heuristic=br --zest-discard-far-states=false"; 
+#$kleeargs = " --zest            --disable-opt                        -debug-print-instructions   --zest-depth-offset=$offset       --use-symbex=2 --symbex-for=10 --search=zest --zest-search-heuristic=br --zest-discard-far-states=false"; 
+
+  if("" ne $dsa) {
+    $kleeargs = $kleeargs .  " --dsa";
+  }
+
 }
 
 
-if(defined($test)) {
+if(defined($test) || defined($testll)) {
 
-#  if($withoutcheker ne "") {
+  if("" ne $test) {
     execute("$make clean");
     execute("$make $test LLVM_BIN=$llvm_bin   KLEEINCLUDE=$kleeincl");
     execute("cp $test.bc $test.a.out.bc");
+  } else {
+    $test = $testll;
+    execute("$llvmas $test.ll -o $test.bc");
+    execute("cp $test.bc $test.a.out.bc");
+  }
 
-    if("" eq $genexec) {
-      &runKlee;
-    }
-    
-    execute("echo");
-    execute("echo");
-    exit(0);
-#  }
-
-
-#  if($modifiedll eq "") {
-#    execute("$make clean");
-#    execute("$make $test LLVM_BIN=$llvm_bin   KLEEINCLUDE=$kleeincl");
-#    execute("echo");
-#    execute("$make $test-kleecheck LLVM_BIN=$llvm_bin LLVMPALIB=$llvmpalib KLEEINCLUDE=$kleeincl JFCHECKERMAP=$palib/jf-rt/ JFINCLUDE=$painclude/");
-#    execute("cp $test-kleecheck.bc $test.a.out.bc");
-#    execute("echo");
-#    execute("echo");
-#  } else {
-#    execute("$llvmas < $test-kleecheck.ll  > $test-kleecheck.bc");
-#    execute("cp $test-kleecheck.bc $test.a.out.bc");
-#  }
-#  if("" eq $genexec) {
-#    &runKlee;
-#  }
+  &runKlee;
   execute("echo");
   execute("echo");
   exit(0);
@@ -153,6 +147,8 @@ if(defined($test)) {
 sub runKlee {
   if(-e "$test.a.out.bc") {
     execute("$kleeexec $kleeargs ./$test.a.out.bc @progargs");
+  } else {
+    execute("echo ");
   }
 }
 
@@ -217,3 +213,22 @@ if("" ne $reuse) {
 #}
     #execute("cat $test-kleecheck.ll | sed 's/target datalayout.*//' | sed 's/\!llvm.ident =.*//' | sed 's/\!0 = metadata.*//' | sed 's/^attributes \#[0-9]*.*//' | sed 's/\#[0-9][0-9]*//' >  temp ");
     #execute("mv temp $test-kleecheck.ll") ;
+
+#  }
+
+
+#  if($modifiedll eq "") {
+#    execute("$make clean");
+#    execute("$make $test LLVM_BIN=$llvm_bin   KLEEINCLUDE=$kleeincl");
+#    execute("echo");
+#    execute("$make $test-kleecheck LLVM_BIN=$llvm_bin LLVMPALIB=$llvmpalib KLEEINCLUDE=$kleeincl JFCHECKERMAP=$palib/jf-rt/ JFINCLUDE=$painclude/");
+#    execute("cp $test-kleecheck.bc $test.a.out.bc");
+#    execute("echo");
+#    execute("echo");
+#  } else {
+#    execute("$llvmas < $test-kleecheck.ll  > $test-kleecheck.bc");
+#    execute("cp $test-kleecheck.bc $test.a.out.bc");
+#  }
+#  if("" eq $enexec) {
+#    &runKlee;
+#  }
