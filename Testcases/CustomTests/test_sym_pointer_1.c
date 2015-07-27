a657a9090308e153cd85923daddc0202a8e502c9
Sandeep Dasgupta <sdasgup3@illinois.edu>
2015-07-27 13:48:39 -0500

    making the pointed area symbolic rather than pointer p itself

--- Testcases/CustomTests/test_sym_pointer_1.c
+++ Testcases/CustomTests/test_sym_pointer_1.c
@@ -2,16 +2,18 @@
 #include<assert.h>
 #include<stdlib.h>
 
+//If you simply make pointer P symbolic, that's exactly the same as a normal variable, i.e., klee_make_symbolic(&P, sizeof(P), "p"), which means that P's value is uncertain.
+//However if you also need to deference it(in most of the cases you do), you've to make the pointed area symbolic rather than P itself;
 
 int main(int argc, char** argv) {
   int x = 3 , y = 5, sum = 0 ;
   int*  p = (int *)malloc(sizeof(int));
 
-  klee_make_symbolic(&p, sizeof(p), "p");
+  klee_make_symbolic(p, sizeof(int), "p");
 
 
 
-  if(0 != p) {
+  if(0 != *p) {
     sum  = 0;
   } else {
     sum  =1;
